b6c97113c2608077464d7d80810c3dec24a99624
Metehan BAŞ <metehan.bas@lokmanecza.local>
2022-11-17 17:54:36 +0300

    hero section height

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Footer from "./components/shared/footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="custom-hero-background h-screen">
+    <div className="custom-hero-background relative h-screen max-h-[850px]">
       <Navbar />
       <Hero />
     </div>
