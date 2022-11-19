17461f378f9448fc5ac23f36b4eae4cfdc999866
Metehan Baş <metehancse@gmail.com>
2022-11-19 19:49:20 +0300

    cart-product-border-green

--- src/components/shared/cart/sidebar/cart-products/index.js
+++ src/components/shared/cart/sidebar/cart-products/index.js
@@ -21,33 +21,32 @@ export const MenuItem = ({ i }) => {
       },
     },
   };
-  const style = { border: `2px solid #FF008C` };
+  const style = { borderBottom: `2px solid #048526` };
   return (
-    <motion.li
-      className="flex gap-4 items-center py-3"
-      style={style}
-      variants={variants}
-    >
-      <motion.div className="cart-product-img">
-        <img src={watch} alt="DNO-NECKLACE" />
-      </motion.div>
-      <div className="">
-        <div className="cart-product-name text-xs w-40 truncate">
-          Gümüş Kordon Erkek Kol Saatidsaadd
-        </div>
-        <div className="flex justify-between">
-          <div className="cart-product-price text-xs">31,31 ₺</div>
-          <div className="cart-product-amount text-xs">1 Adet</div>
-        </div>
-      </div>
-      <div className="flex flex-col pr-4 gap-2 text-2xl">
-        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}>
-          <AiOutlinePlusCircle />
+    <>
+      <motion.li className="flex gap-4 items-center py-3" variants={variants}>
+        <motion.div className="cart-product-img">
+          <img src={watch} alt="DNO-NECKLACE" />
         </motion.div>
-        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}>
-          <AiOutlineMinusCircle />
-        </motion.div>
-      </div>
-    </motion.li>
+        <div className="">
+          <div className="cart-product-name text-xs w-40 truncate">
+            Gümüş Kordon Erkek Kol Saatidsaadd
+          </div>
+          <div className="flex justify-between">
+            <div className="cart-product-price text-xs">31,31 ₺</div>
+            <div className="cart-product-amount text-xs">1 Adet</div>
+          </div>
+        </div>
+        <div className="flex flex-col pr-4 gap-2 text-2xl">
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}>
+            <AiOutlinePlusCircle />
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }}>
+            <AiOutlineMinusCircle />
+          </motion.div>
+        </div>
+      </motion.li>
+      <div style={style} className="w-4/5 mx-auto "></div>
+    </>
   );
 };
